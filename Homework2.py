1bf71afa7b17d7c62a379392166abcd55e3026eb
Maximovex <maximove81@gmail.com>
2024-07-05 19:01:19 +0300

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,5 @@
+tasks_num=12
+hr_taken=1.5
+course_name='Python'
+time_for_one_task=hr_taken/tasks_num
+print('Курс:',course_name,', всего задач:',tasks_num,' затрачено часов:',hr_taken,', среднее время выполнения',time_for_one_task,' часа')
