5d5669e964e12f6dccbbb2d1bddaeeb331606dd5
Maximovex <maximove81@gmail.com>
2024-07-06 17:45:49 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,7 @@
+immutable_var=5,'Hello',26,True
+print(immutable_var)
+#immutable_var[0]=10
+#нельзя изменять значения элементов кортежа напрямую, основное свойство данной переменной
+mutable_list=['world',10,'hello']
+mutable_list[0]='Earth'
+print(mutable_list)
