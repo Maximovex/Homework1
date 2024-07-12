ce817df1d49eea5e83dc156b1e4dc9a1d49dfe6b
Maximovex <maximove81@gmail.com>
2024-07-12 18:25:14 +0300

    Add files via upload

--- module1hard.py
+++ module1hard.py
@@ -1,15 +1,15 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-grades=[int(sum(grades[0])/len(grades[0])),
-        int(sum(grades[1])/len(grades[1])),
-        int(sum(grades[2])/len(grades[2])),
-        int(sum(grades[3])/len(grades[3])),
-        int(sum(grades[4])/len(grades[4]))]
+grades=[sum(grades[0])/len(grades[0]),
+        sum(grades[1])/len(grades[1]),
+        sum(grades[2])/len(grades[2]),
+        sum(grades[3])/len(grades[3]),
+        sum(grades[4])/len(grades[4])]
 print(grades)
 students=sorted(list(students))
 students_grades={students[0]:grades[0],
                  students[1]:grades[1],
                  students[2]:grades[2],
                  students[3]:grades[3],
-                 students[4]:grades[4],}
+                 students[4]:grades[4]}
 print(students_grades)
